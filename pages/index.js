44b958bd2a63722f0c76e7f64491404133c9d9ca
Dixit Desai <85556276+dixitdesai@users.noreply.github.com>
2021-11-30 23:46:39 +0530

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -3,7 +3,7 @@ import Image from "next/image"
 import { Flex, Box, Text, Button } from "@chakra-ui/react"
 
 import { baseUrl, fetchApi } from "../utils/fetchApi"
-import Property from "../components/property"
+import Property from "../components/Property"
 
 export const Banner = ({ purpose, title1, title2, desc1, desc2, buttonText, linkName, imageUrl }) => (
   <Flex flexWrap='wrap' justifyContent='center' alignItems='center' m='10'>
